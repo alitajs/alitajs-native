693df04c7b552ea435d1dc1e9ce0529f411fe2ee
chen.yaoxing <504554911@qq.com>
2022-01-24 10:37:27 +0800

    maven central发布配置

--- android/Example/app/src/main/java/com/alitajs/example/MainActivity.java
+++ android/Example/app/src/main/java/com/alitajs/example/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.alitajs.capacitorandroid.AlitaJsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
